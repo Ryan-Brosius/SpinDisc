f50188fcdf8a04b921aefa94d64e99ae27250bef
sandvich-dispenser <thedvshowchannel@gmail.com>
2026-06-21 22:55:43 -0400

    Basic Tower Scriptable Objects

---
+++ Assets/Scripts/Towers/TowerSO.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TowerSO", menuName = "Scriptable Objects/TowerSO")]
+public class TowerSO : ScriptableObject
+{
+    public string Name;
+    
+    public float Damage;
+
+    public float Range;
+
+    public float FireRate;
+
+    [Range(0f,360f)]
+    public float Spread;
+
+    public float Cost;
+}
